8160e172a920d3ddb1d92645e079fef397d65ff0
vbazylevych <608levlerka>
2017-10-16 23:44:19 +0300

    HW3 fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,68 +1,78 @@
 import java.io.*;
 import java.nio.file.*;
-import static org.apache.commons.io.IOUtils.*;
+import java.util.*;
 
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-
         Path directory = Paths.get("testfiles");
 
         if (Files.exists(directory) && Files.isDirectory(directory)) {
 
             StringBuilder stringBuilder = new StringBuilder();
-            String oneLineText = "";
-            int generalSum = 0;
+            Map<String, Integer> result = new HashMap<>();
 
             DirectoryStream<Path> files = Files.newDirectoryStream(directory);
 
             for (Path file : files) {
+                TextUtils.printFileAtributes(file);
+                List<String> lines = Files.readAllLines(Paths.get(file.toString()));
 
-                try (BufferedReader reader = new BufferedReader(
-                        new InputStreamReader(
-                                new FileInputStream(file.toString())))) {
-                    String line;
-
-                    while ((line = reader.readLine()) != null) {
-
-                        oneLineText = stringBuilder.append(line).append(" ").toString();
-                    }
-
-                    TextUtils.printFileAtributes(file);
-
-                    int oneFileSum = TextUtils.getOneFileSum(oneLineText);
-                    generalSum = generalSum + oneFileSum;
-                    System.out.println("Sum of frequency for file " + file.getFileName() + " -- " + oneFileSum + "\n");
-
-                    stringBuilder = new StringBuilder();
-
-                } catch (IOException e) {
-                    System.out.println("I cant find such directory or can't read from it");
+                for (String line : lines) {
+                    stringBuilder.append(line).append(" ");
                 }
 
+                Map<String, Integer> frequency = new Text(stringBuilder.toString()).getWordFrequencies();
+                stringBuilder = new StringBuilder();
+
+                result = bigMap(result, frequency);
             }
-            System.out.println("Sum of frequency for ALL files:  " + generalSum + "\n");
+
+            System.out.println(result);
+
         } else {
             System.out.println("It isn't directory");
         }
 
+        myCopyFile("input.txt", "output.txt");
+    }
 
-        try {
-            InputStream input = new FileInputStream("input.txt");
-            OutputStream output = new FileOutputStream("output.txt");
-            copy(input, output, 1024);
-            input.close();
-            output.close();
+    private static void myCopyFile(String originName, String copyName) {
+        try (InputStream input = new FileInputStream(originName);
+             OutputStream output = new FileOutputStream(copyName)) {
+
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = input.read(buffer)) > 0) {
+                output.write(buffer, 0, length);
+            }
             System.out.println("FILE WAS COPIED!");
         } catch (FileNotFoundException e) {
             System.out.println("Cant find file " + e.getMessage());
         } catch (IOException e) {
             System.out.println("Can't copy");
         }
-
     }
 
+    public static Map<String, Integer> bigMap(Map<String, Integer> map1, Map<String, Integer> map2) {
 
+        Map<String, Integer> result = new HashMap<String, Integer>();
+        Set<String> allKeys = new HashSet<String>();
+        allKeys.addAll(map1.keySet());
+        allKeys.addAll(map2.keySet());
+
+        for (String key : allKeys) {
+
+            if (map1.get(key) != null && map2.get(key) != null) {
+                result.put(key, map1.get(key) + map2.get(key));
+            } else if (map1.get(key) == null) {
+                result.put(key, map2.get(key));
+            } else {
+                result.put(key, map1.get(key));
+            }
+        }
+        return result;
+    }
 }
